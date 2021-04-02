383715ef520d13892d906d3410985056feaccaa4
Webster Massingham <wmassingham@gmail.com>
2021-04-01 20:30:53 -0700

    update

--- banner.sh
+++ banner.sh
@@ -5,18 +5,16 @@ outfile=$(echo "$infile" | sed -e 's/_diffuse.png$/.dds/' -e 's/_normal.png/_n.d
 
 echo "converting $infile"
 echo "to $outfile"
-# magick convert $infile -gravity northeast -crop 2048x4096+1024+0 +repage $outfile
-# magick convert $outfile -format dds -define dds:compression=dxt5 $outfile.replace('.png','.dds')
-
-rm $outfile
 
 convert "$infile" \
-    -gravity northeast \
-    -background transparent \
-    -crop 2048x4096+1024+0!   +repage \
-    -resize 1744x4096+304+0!  +repage \
-    -extent 2048x4096+0+0! +repage \
-    -format dds -define dds:compression=dxt5 "$outfile"
-#rm "$infile"
+	-gravity northeast \
+	-background transparent \
+	-crop 2048x4096+1024+0!   +repage \
+	-resize 1744x4096+304+0!  +repage \
+	-extent 2048x4096+0+0! +repage \
+	-format dds -define dds:compression=dxt5 "$outfile"
 
-# read
+# if it's a normal map, halve the resolution
+if [[ $infile == *_normal.png ]]; then mogrify -resize 50% $outfile; fi
+
+rm "$infile"
